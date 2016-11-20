ea9cd4b5443b5b52064420d79ecfa1d9204e4a4c
dalekfrog <dalek.frog@gmail.com>
2016-11-20 16:27:47 +0800

    3 pt

--- get-ttg.py
+++ get-ttg.py
@@ -23,7 +23,6 @@ ttg_pages = urllib2.urlopen(ttg_req)
 #print(ttg_pages.info().get('Content-Encoding'))
 
 if ttg_pages.info().get('Content-Encoding') == 'gzip':
-    #print ttg_pages.read()
     buf = StringIO.StringIO(ttg_pages.read())
     f = gzip.GzipFile(fileobj=buf)
     ttg_html = f.read()
@@ -37,36 +36,18 @@ ttg_fulltable = ttg_soup.find("table",{"id" : "torrent_table"})
 ttg_torrents = []
 for row in ttg_fulltable.find_all("tr",class_=["hover_hr  sticky","hover_hr"],recursive=False):
     id = row["id"]
-    torrent_fix = row.find("td",{"align":"left"}).find("div").a.b #.find("br").next_sibling
-    #print torrent_fix.contents[0].strip()
-    #print torrent_fix.span.text
-    #print str(torrent_fix).replace("<br/>","\n")
-    #print re.search(r'(?<=>)*(?=<)', torrent_fix ).group()
-    #print torrent_fix.find("br").next_sibling
-    #if str(type(torrent_fix.find("br").next_sibling)) == '<class \'bs4.element.Tag\'>' :
-    #if (torrent_fix.find("br").next_sibling) :
-    #    print torrent_fix.find("br").next_sibling.text
-    #    title = torrent_fix.find("br").next_sibling
-    #else:
-    #    title = ''
-    #title = torrent_fix.text[0]
     size = row.find_all("td",{"align":"center"})[3].text
-    print id
-    print size
-    #print title
-#    torrent_fix = row.find("td",{"class":"t_name"}).find("tr").find_all("td")[1]
-#    torrent_misc = torrent_fix.h3.a
-#    title = torrent_misc['title']
-#    id = re.search(r'\d+(?=&)',torrent_misc['href']).group()
-#    name = torrent_fix.h4.text
-#    size = row.find("td",{"class":"t_size"}).text
-#    #print title
-#    #print name
-#    #print id
-#    #print size
-#    torrent = [title,name,id,size]
-#    ttg_torrents.extend([torrent])
-#
-#r
-#print tt_torrents
+    torrent_fix = str(row.find("td",{"align":"left"}).find("div").a.b).split('<br/>')   #.find("br").next_sibling
+    title = re.sub(r'</?\w+[^>]*>','',torrent_fix[0])
+    try:
+        name = re.sub(r'</?\w+[^>]*>','',torrent_fix[1]).strip()
+    except:
+        name = ''
+
+    torrent = ['TTG',title,name,id,size]
+    ttg_torrents.extend([torrent])
+
+
+
+print ttg_torrents
 

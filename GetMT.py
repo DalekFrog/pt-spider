b3bd4a0d286e0f1a8dc9a943cdd3810a1b3a92e4
dalekfrog <dalek.frog@gmail.com>
2017-02-19 21:36:37 +0800

    insert db

--- GetMT.py
+++ GetMT.py
@@ -3,6 +3,8 @@
 from bs4 import BeautifulSoup
 import re
 import requests
+import time
+import sqlite3
 
 
 headers = {
@@ -24,29 +26,40 @@ f.close()
 s = requests.Session()
 s.headers.update(headers)
 
+conn = sqlite3.connect('test.db')
+conn.text_factory = str
 
 def showtop():
+    site = 'MT'
     r = s.get(t_url)
     soup =  BeautifulSoup(r.content, "lxml")
     fulltable = soup.find("table",{"class" : "torrents"})
-    mt_torrents = []
+    cur = conn.cursor()
+    #mt_torrents = []
     for row in fulltable.find_all("tr",class_=['sticky_top','sticky_normal'],recursive=False):
-    	torrent_other = row.find_all("td",{"class":"rowfollow"})
-    	size = torrent_other[3].text
-    	torrent_table = row.find("table",{"class":"torrentname"})
-    	torrent_img = torrent_table.find("td",{"class":"torrentimg"}).a.find("img")['src']
-    	torrent_fix = torrent_table.find("td",{"class":"embedded"})
-    	title = torrent_fix.a['title']
-    	try:
-    	    name = torrent_fix.find('br').nextSibling
-    	except:
-    	    name = ''
-    	id = re.search(r'\d+(?=&)', torrent_fix.a['href'] ).group()
+        torrent_other = row.find_all("td",{"class":"rowfollow"})
+        size = torrent_other[3].text
+        up_time = torrent_other[2].span['title']
+        upltime = int(time.mktime(time.strptime(up_time,"%Y-%m-%d %H:%M:%S")))
+        torrent_table = row.find("table",{"class":"torrentname"})
+        torrent_img = torrent_table.find("td",{"class":"torrentimg"}).a.find("img")['src']
+        torrent_fix = torrent_table.find("td",{"class":"embedded"})
+       	t_title = torrent_fix.a['title']
+       	try:
+       	    name = torrent_fix.find('br').nextSibling
+       	except:
+       	    name = ''
+       	site_id = re.search(r'\d+(?=&)', torrent_fix.a['href'] ).group()
 
-    	per_torrent = ['MT',title,name,id,size]
-    	mt_torrents.extend([per_torrent])
-    return mt_torrents
+        t_link = "https://tp.m-team.cc/details.php?id=" + site_id + "&hit=1"
+        d_link = "https://tp.m-team.cc/download.php?id=" +site_id + "&passkey=" + "mypasskey" + "&https=1"
+
+        cur.execute("INSERT INTO PTTOP (SITE,TITLE,NAME,SITE_ID,SIZE,T_LINK,D_LINK,UPLTIME ) VALUES (?,?,?,?,?,?,?,?) " ,  (site,t_title,name,site_id,size,t_link,d_link,upltime))
+        conn.commit()
+        #per_torrent = ['MT',t_title,name,id,size,upltime]
+        #mt_torrents.extend([per_torrent])
+    #return mt_torrents
 
 if __name__ == '__main__':
-    print showtop()
+    showtop()
 
